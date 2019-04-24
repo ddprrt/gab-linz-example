fcc92f36f9399b63e69f59adff86a9f809da447d
Baumgartner <sbaumg@gmail.com>
2019-04-24 15:55:50 +0200

    wave

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Layout: React.FunctionComponent<Props> = ({ children, title = 'This is the default title' }) => (
   <div>
     <Head>
-      <title>{title}</title>
+      <title>👋 {title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
