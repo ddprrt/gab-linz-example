b2053d146a996bf5f77fa3bc626ff5d45fc6a448
Baumgartner <sbaumg@gmail.com>
2019-04-27 10:37:07 +0200

    Add Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Layout: React.FunctionComponent<Props> = ({ children, title = 'This is the default title' }) => (
   <div>
     <Head>
-      <title>👋 {title}</title>
+      <title>{title} 🖖</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
