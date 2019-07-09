6d0a4611a1632078a1d7c85e08efafc64b952d25
Stefan Baumgartner <sbaumg@gmail.com>
2019-07-09 20:59:12 +0200

    live from dme

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1>Hello DME001 👋</h1>
       <p><Link href='/about'><a>About</a></Link></p>
     </Layout>
   )
